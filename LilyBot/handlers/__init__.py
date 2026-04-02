2c329763f6284e2c7f952cd28b5238f02a0281fc
ᴀʏᴜsʜʜ ᴘᴀʀᴋ <maharanachinmaya79@gmail.com>
2026-04-02 19:05:41 +0530

    Update __init__.py

--- LilyBot/handlers/__init__.py
+++ LilyBot/handlers/__init__.py
@@ -1,5 +1,5 @@
 # handlers/__init__.py  –  Marks this directory as a Python package
 
-from handlers import admin, welcome, filters, warnings, antispam, notes, rules, locks, owner, start
+from handlers import admin, welcome, filters, warnings, antispam, notes, rules, locks, owner, start, auto_reply.py
 
-__all__ = ["admin", "welcome", "filters", "warnings", "antispam", "notes", "rules", "locks", "owner", "start"]
+__all__ = ["admin", "welcome", "filters", "warnings", "antispam", "notes", "rules", "locks", "owner", "start", "auto_reply.py"]
